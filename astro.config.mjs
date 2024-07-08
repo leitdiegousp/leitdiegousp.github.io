8855d1874d4cd8d035009abbd0faf3be9b2df8ee
leitdiegousp <152520462+leitdiegousp@users.noreply.github.com>
2024-07-08 13:21:59 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import metaTags from "astro-meta-tags";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://skncre-cosmetics-hygraph-astro.vercel.app",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    metaTags(),
-    sitemap(),
-  ],
-  image: {
-    domains: ["https://media.graphassets.com"],
-  },
+  site: 'https://leitdiegousp.github.io/cosmetic_page', // Substitua "leitdiegousp" pelo seu nome de usuário do GitHub e "cosmetic_page" pelo nome do seu repositório
+  base: '/cosmetic_page/', // Substitua "cosmetic_page" pelo nome do seu repositório
 });
