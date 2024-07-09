65affb4e5660955b4a3f31d1dbf82e426cd734d1
leitdiegousp <152520462+leitdiegousp@users.noreply.github.com>
2024-07-09 18:33:15 -0300

    alteração fontes and  tailwind add postcss.config.cjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        light: "#fff",
-        dark: "#333333",
-        primary: "#693A25",
-        secondary: "#AE9A91",
-        tertiary: "#EEE0DB",
-      },
       fontFamily: {
-        sans: ["Inter"],
-        title: ["Plus Jakarta Sans"],
+        sans: ['Inter', 'Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+        jakarta: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary': '#your-primary-color',
+        'secondary': '#your-secondary-color',
+        'tertiary': '#your-tertiary-color',
+        'light': '#your-light-color',
+        'dark': '#your-dark-color',
       },
     },
   },
   plugins: [],
-};
+}
